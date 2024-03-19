46bad7211f9a22b98ea802f8730360de9ec74f5c
ThomasVrd11 <Thomasviard@orange.fr>
2024-03-19 20:53:33 +0100

    Update assembly version in formula1 project

--- formula1/obj/Debug/net8.0/formula1.AssemblyInfo.cs
+++ formula1/obj/Debug/net8.0/formula1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("formula1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cade09a3379664ef2027ffba303a5cb1a9d81ae3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d701db8f67a297bfd3975153e95ca4225c58a228")]
 [assembly: System.Reflection.AssemblyProductAttribute("formula1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("formula1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
